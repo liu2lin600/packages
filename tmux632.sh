4398877219dbe727aecbc93178f520bf42846cff
liu2lin600 <liu2lin600@gmail.com>
2017-03-18 14:52:19 +0800

    Add files via upload

---
+++ tmux632.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# tmux2.3 on centos6.x86
+
+pack='tmux-2.3-centos6.x86.tar.gz'
+local='/usr/local/tmux'
+
+curl -O 23.106.147.189/$pack
+tar xf $pack -C /usr/local/
+rm -f $pack
+ln -s /usr/local/tmux/bin/tmux /usr/bin
+
+cp $local/conf/tmux.conf ~/.tmux.conf
+cp $local/conf/tmux.conf.local ~/.tmux.conf.local
+
+cp $local/lib/libevent-2.0.so.5 /usr/lib
+
+tmux -V
